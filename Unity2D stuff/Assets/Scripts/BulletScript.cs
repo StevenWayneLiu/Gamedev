5ff3318d4472195979d1148d8c5b58ec756de01d
StevenWayneLiu <shadowth123@yahoo.com>
2015-04-07 19:40:00 -0400

    4/7/2015
    Progress:
    -added enemy bullets
    -generalized enemy move script so that child scripts can be branched off
    from it and derived from it.
    Short-Term Goals:
    Notes:

--- Unity2D stuff/Assets/Scripts/BulletScript.cs
+++ Unity2D stuff/Assets/Scripts/BulletScript.cs
@@ -2,49 +2,51 @@
 using System.Collections;
 
 public class BulletScript : MonoBehaviour {
-    public float velocity = 10f;
-    private float travel;
-    private int damageVal = 10;//damage value for projectile
-    private Vector3 buff;
-    private bool isFired = false;
-    private float start;
-    private float maxDist = 10f;//max distance that a bullet can fly
-	// Use this for initialization
-	void Start () {
-        buff = gameObject.transform.position;
-	}
+    protected float xVel = 15f;
+    protected float yVel = 0f;
+    protected Vector2 travel;//keeps track of distance traveled from start position
+    protected int damageVal = 10;//damage value for projectile
+    protected Vector2 initPos;
+
+    protected float lifeTime = 2.0f;
+
+    protected void OnEnable()
+    {
+        Invoke("Deactivate", lifeTime);
+        initPos = gameObject.transform.position;
+    }
+    protected void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+    protected void OnDisable()
+    {
+        CancelInvoke();
+    }
 
     public int getDamage()
     {
         return damageVal;
     }
-    //change fired state of bullet and move it to a target location
-    public void setFired(bool fired,Vector3 pos)
-    {
-        isFired = fired;//change the fired state
-        gameObject.transform.position = pos;//move bullet to corresponding position
-        start = pos.x;//set start position for bullets to x
-    }
 
 	// Update is called once per frame
-	void Update () {
-        travel = gameObject.transform.position.x - start;//keep track of distance traveled from start position
-        if (Mathf.Abs(travel) > maxDist)//if bullet reaches the maximum distance
-            setFired(false,buff);//deactivate bullet and move it to the buffer
+	protected void Update () {
+        float xDist = gameObject.transform.position.x - initPos.x;
+        float yDist = gameObject.transform.position.y - initPos.y;
+        travel = new Vector2(xDist, yDist);//keep track of distance traveled from start position
 	}
+
     void FixedUpdate()
     {
-        if(isFired)
-            GetComponent<Rigidbody2D>().velocity = new Vector2(velocity,0f);
-        else
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xVel, yVel);
     }
-    void OnTriggerEnter2D(Collider2D collider)//do stuff when bullet hits target
+
+    protected void OnTriggerEnter2D(Collider2D collider)//do stuff when bullet hits target
     {
         GameObject entity = collider.gameObject;//find gameObject for collider hit
-        if (entity.tag == "Entity" && isFired)//if gameObject hit isn't a bullet and the bullet is active
+        if (entity.tag == "Entity")//if gameObject hit isn't a bullet and the bullet is active
         {
-            setFired(false,buff);//deactivate bullet and move to buffer
+            Deactivate();//deactivate bullet
         }
     }
     
--- Unity2D stuff/Assets/Scripts/EnemyMoveScript.cs
+++ Unity2D stuff/Assets/Scripts/EnemyMoveScript.cs
@@ -2,29 +2,8 @@
 using System.Collections;
 
 public class EnemyMoveScript : MonoBehaviour {
-    private float time = 0f;
-    private float xVel = 0f;
-    private float yVel = 5f;
-    public float switchTime = 1f;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-    //AI movement
-    void FixedUpdate()
-    {
-        time += Time.deltaTime;
-        if (time > switchTime)
-        {
-            yVel *= -1f;
-            time = 0f;
-        }
-        GetComponent<Rigidbody2D>().velocity = new Vector2(xVel, yVel);
-    }
-
+    protected float time = 0f;
+    protected float xVel = 0f;
+    protected float yVel = 0f;
+    
 }
