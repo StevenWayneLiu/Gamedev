6deb93be0ceb208d2edd72b153604f37a24cf41b
StevenWayneLiu <shadowth123@yahoo.com>
2015-07-08 05:10:00 -0400

    7/8/2015
    Wrote a script that allows persistence between scenes, as well as basic
    saving and loading functionality. Format of save data will depend on the
    game

---
+++ Unity3D stuff/Assets/Custom Assets/Scripts/Turn-Based Rpg/GameData.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+public class GameData : MonoBehaviour {
+
+    public static GameData data;
+
+    public float health;
+    public float experience;
+
+	// Use this for initialization
+	void Awake () {
+
+        //check for existing gamedata object
+	    if(data == null)//if there's no existing gamedata object
+        {
+            DontDestroyOnLoad(gameObject);//persist this object between scenes
+            data = this;//set static reference to this gamedata object so it can be globally accessed
+        }
+        else if(data != this)//if there's already a gamedata object and this isn't it
+        {
+            Destroy(gameObject);//destroy this one
+        }
+	}
+
+    //saving function
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file;
+        PlayerData pd;
+        //open file for saving stuff
+        if(File.Exists(Application.persistentDataPath + "/gameData.dat"))//if save file already exists
+        {
+            //open file
+            file = File.Open(Application.persistentDataPath + "/gameData.dat", FileMode.Open);
+            pd = (PlayerData)bf.Deserialize(file);//retrieve playerdata object from file 
+        }
+        else//else create a new save file
+        {
+            file = File.Create(Application.persistentDataPath + "/gameData.dat");
+            pd = new PlayerData();//create a new PlayerData class to contain the data for serialization
+        }
+        
+        //write data in this session into class to be saved
+        pd.health = health;
+        pd.experience = experience;
+
+        bf.Serialize(file, pd);//write PlayerData to file location
+        file.Close();//close filestream after done
+    }
+
+    //loading function
+    public void Load()
+    {
+        if(File.Exists(Application.persistentDataPath + "/gameData.dat"))//if save file already exists
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameData.dat", FileMode.Open);
+            PlayerData pd = (PlayerData)bf.Deserialize(file);//retrieve playerdata object from file
+            file.Close();
+
+            health = pd.health;
+            experience = pd.experience;
+        }
+    }
+}
+
+//class for holding the actual data when saving and loading
+[Serializable]//allows the class to be serialized, and therefore saved to a file in binary format
+class PlayerData
+{
+    public float health;
+    public float experience;
+}
---
+++ Unity3D stuff/Assets/Custom Assets/Scripts/Turn-Based Rpg/TBBattleSystem.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TBBattleSystem : MonoBehaviour {
+
+    private List<GameObject> PlayerList;//stores player characters
+    private List<GameObject> EnemyList;//stores enemy
+    private List<GameObject> TurnOrder = new List<GameObject>();//stores the turn order of list
+    private int OrderSize;//number of turns ahead that are shown
+    private bool NextTurn = false;//true if player has taken their turn
+
+	// Use this for initialization
+	void Start () {
+	    //add all entities to turn order
+        //go to first character's turn
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //check to see if character has taken their turn
+        if (NextTurn)//If character has taken their turn,
+        {
+            //check to see if battle is over
+            if (BattleWon() || BattleLost())
+            {
+                //if so, end battle
+                BattleEnd();
+            }
+            else//else continue with battle
+            {
+                //recalculate turn order based on speed
+                //check turn list and go to next character's turn
+            }
+            
+        }
+        
+	}
+
+    //ends battle, should only be called if either BattleWon() or BattleLost() is true
+    void BattleEnd()
+    {
+        if(BattleWon())
+        {
+            //award end-of-battle stuff to player
+        }
+        else if(BattleLost())
+        {
+            //game over stuff
+        }
+    }
+    //returns ture if all the enemies are defeated
+    bool BattleWon()
+    {
+        if (EnemyList.Count == 0)//
+            return true;//if all enemies are dead battle is won
+        else
+            return false;//else return false
+    }
+    //returns true if all the player's characters are dead
+    bool BattleLost()
+    {
+        bool allDead = true;
+        for(int i = 0; i < PlayerList.Count; i++)
+        {
+            //if character's hp is greater than zero
+            //allDead becomes false
+        }
+        return allDead;
+    }
+}
